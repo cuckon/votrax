5c1b39d3304f0b0f56c8522ffff6be563ecf8ade
John <cuckons@gmail.com>
2021-07-04 23:17:26 +0800

    more complete api

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import os
 import subprocess
 import hashlib
 import logging
+import asyncio
+from functools import lru_cache
 from typing import Optional
 from pathlib import Path
 
@@ -38,6 +40,18 @@ def purge_files():
         audio_files[i].unlink()
 
 
+@lru_cache()
+def get_voice_list():
+    url = f'https://{REGION}.tts.speech.microsoft.com/cognitiveservices/voices/list'
+    info = requests.get(url, headers=HEADERS).json()
+
+    keys = ['ShortName', 'Gender', 'StyleList', 'LocalName']
+    return [
+        {k:i[k] for k in keys if k in i }
+        for i in info
+    ]
+
+
 def play_mp3(path):
     subprocess.Popen(['mpg123', '-q', path])
 
@@ -47,7 +61,16 @@ def hashed_file_name(text:str):
     return hashlib.md5(text.encode('utf-8')).hexdigest() + '.mp3'
 
 
-def _azure_synthesis(text, name, style, gender):
+def _get_gender_by_name(name):
+    for i in get_voice_list():
+        if i['ShortName'] == name:
+            return i['Gender']
+    return 'Female'
+
+
+def _azure_synthesis(text, name, style):
+    voice_list = get_voice_list()
+    gender = _get_gender_by_name(name)
     raw = f"""
     <speak xmlns="http://www.w3.org/2001/10/synthesis"
         xmlns:mstts="http://www.w3.org/2001/mstts"
@@ -67,7 +90,8 @@ def _azure_synthesis(text, name, style, gender):
     r = requests.post(url, data=raw, headers=HEADERS)
     return r.content
 
-def text_to_mp3(text:str, name, style, gender) -> Path:
+
+async def text_to_mp3(text:str, name, style) -> Path:
     """Convert text to mp3 file."""
     if not text:
         return
@@ -75,13 +99,12 @@ def text_to_mp3(text:str, name, style, gender) -> Path:
     if not MP3_FOLDER.exists():
         MP3_FOLDER.mkdir()
 
-    path = MP3_FOLDER / hashed_file_name(text + name + style + gender)
+    path = MP3_FOLDER / hashed_file_name(text + name + style)
     if path.exists():
         path.touch()
         return path
 
-
-    result = _azure_synthesis(text, name, style, gender)
+    result = _azure_synthesis(text, name, style)
 
     path.write_bytes(result)
     purge_files()
@@ -90,40 +113,76 @@ def text_to_mp3(text:str, name, style, gender) -> Path:
     logging.error('Failed to convert text:' + result)
 
 
-def make_html(payload):
+async def make_html(payload):
     return f'''
     <head><title>机器猫广播站</title></head>
-    <body>{payload}</body>
+    <body>
+        <div>
+            {payload}
+        </div>
+        <hr>
+        <div>
+            给机器猫转账，换更大的喇叭:<br>
+            <img src="/static/qrcode.png" width="160" height="160">
+        </div>
+
+    </body>
     '''
 
 
+async def convert_and_play(text, name, style):
+    mp3_path = await text_to_mp3(text, name, style)
+    if mp3_path:
+        play_mp3(str(mp3_path))
+    return mp3_path
+
+
 @app.get("/v1/{text}", response_class=HTMLResponse)
-def speak(
+async def speak(
         text: str,
         name: Optional[str]='zh-CN-XiaoxiaoNeural',
         style: Optional[str]='affectionate',
-        gender: Optional[str]='Female',
     ):
     """Main entrance."""
-    mp3_path = text_to_mp3(text, name, style, gender)
-    if mp3_path:
-        play_mp3(str(mp3_path))
-    return make_html(f'已广播: "{text}"')
+    res = await asyncio.gather(
+        convert_and_play(text, name, style),
+        make_html(f'已广播: "<B>{text}</B>"'),
+    )
+
+    return res[1]
 
 
 @app.get("/v1", response_class=HTMLResponse)
-def help():
+async def get_list():
     """Get available parameters."""
     from json2html import json2html
+    simplified = get_voice_list()
 
-    url = f'https://{REGION}.tts.speech.microsoft.com/cognitiveservices/voices/list'
-    info = requests.get(url, headers=HEADERS).json()
-
-    keys = ['ShortName', 'Gender', 'StyleList']
-    simplified = [
-        {k: i[k]}
-        for i in info
-        for k in keys if k in i
-    ]
     html = json2html.convert(json=simplified)
-    return make_html(html)
+    return await make_html(html)
+
+
+@app.get("/", response_class=HTMLResponse)
+async def help():
+    """Get available parameters."""
+    html = '''
+    <h1>机器猫广播站</h1>
+    <p>浏览器直接访问网址即可广播文字。可用于强提醒、广播找人等情景。
+    <p>可用范围为所有内网的设备，比如电脑、手机、iPad。
+    <p><b>广播源暂时不设限，半匿名（后台可跟踪），请遵纪守法，勿广播不该广播的内容。</b>
+    <hr>
+    <h2>使用范例</h2>
+    <p><a href="javascript:void(0)">http://10.240.154.195:8000/v1/周杰伦看一眼泡泡</a><br>
+    播放"周杰伦看一眼泡泡"
+    <p><a href="javascript:void(0)">http://10.240.154.195:8000/v1/周杰伦看一眼泡泡?name=zh-CN-YunyeNeural</a><br>
+    播放"周杰伦看一眼泡泡"，使用“云野”的声音。
+    <p><a href="javascript:void(0)">http://10.240.154.195:8000/v1/周杰伦看一眼泡泡?style=angry</a><br>
+    播放"周杰伦看一眼泡泡"，使用生气的语调。
+    <p><a href="javascript:void(0)">http://10.240.154.195:8000/v1/周杰伦看一眼泡泡?name=zh-CN-YunyeNeural&style=angry</a><br>
+    播放"周杰伦看一眼泡泡"，使用“云野”的声音，配合生气的语调。
+    <h2>可用声音和语调参考</h2>
+    <p>在<a href="/v1">此页面</a>查看列表。
+    <p>在<a href="https://azure.microsoft.com/zh-cn/services/cognitive-services/text-to-speech/">微软 Azure官方</a>
+    可试听.
+    '''
+    return await make_html(html)
